c41fd999da89047d0286cd72c365fa99f36c6d5c
SETTER <lphp@mail.ru>
2017-02-04 12:07:37 +0300

    Edit gitignore

--- config/conf.js
+++ config/conf.js
@@ -1,14 +1,3 @@
 module.exports.conf = {
-    sendMail: {
-        service: 'gmail',
-        auth: {
-            user: 'apptest387@gmail.com', pass: 'Code:-123000'
-        },
-        mail: {
-            from: 'tester@gmail.com',
-            to: ['lphp@mail.ru', 'apetrov@landata.ru'],
-            subject: 'Регистрация',
-            text: "Вы успешно зарегистрированы."
-        }
-    }
+    
 };
