ff20ecd9df6138ed35013a921d72f950fd889305
SETTER <info@lphp.ru>
2017-04-25 18:20:26 +0300

    EF

--- assets/js/private/admin/users/ctrl/EditController.js
+++ assets/js/private/admin/users/ctrl/EditController.js
@@ -2,9 +2,7 @@ angular.module('UserModule')
     .controller('EditController', ['$scope', '$http', 'toastr', '$state', 'Users', 'Positions', 'Departments', '$stateParams', '$rootScope',
         function ($scope, $http, toastr, $state, Users, Positions, Departments, $stateParams, $rootScope) {
             $scope.me = window.SAILS_LOCALS.me;
-            //if (!$scope.me.admin && !$scope.me.kadr) $state.go('home.admin.users');
-
-
+            if (!$scope.me.admin && !$scope.me.kadr) $state.go('home.admin.users');
 
             $scope.close = 1;
             $scope.loginAdmin = false;
