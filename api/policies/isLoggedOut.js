e875345805044296dc97a7ad9c7e3564c342e9d8
SETTER <info@lphp.ru>
2017-08-17 13:31:58 +0300

    Editing rights

--- api/policies/isLoggedOut.js
+++ api/policies/isLoggedOut.js
@@ -5,7 +5,7 @@ module.exports = function isLoggedOut(req, res, next) {
     }
 
     if (req.wantsJSON) {
-        return res.forbidden('isLoggedOut: Вам не разрешено выполнять это действие.');
+        return res.forbidden('isLoggedOut: Вам не разрешено выполнять это действие. Выйдите сначало из системы.');
     }
 
     return res.redirect('/');
